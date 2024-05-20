6a2b944579f369bcceca16cb284d2f4bac933099
ShadyMate <luisboehler@proton.me>
2024-05-20 20:51:24 +0200

    Third test added (Luis)

--- spec/dicegame_spec.js
+++ spec/dicegame_spec.js
@@ -13,4 +13,9 @@ describe('Dice', function() {
 	it('should return 4 for [ 2, 2, 3, 3 ]', function() {
 		expect(dice.add(array2)).toEqual(4);
 	});
+	// Test Case 3:
+	let array3 = [6, 6, 4, 4, 1, 3];
+	it('should return 2 for [ 6, 6, 4, 4, 1, 3 ]', function() {
+		expect(dice.add(array3)).toEqual(0);
+	});
 });
