90c101956c8a78a1484d787c7eb037bbebf59cea
nbrandtner-tgm <nbrandtner@gmx.at>
2024-05-20 20:45:43 +0200

    refactor test case 2 (Niklas)

--- src/dicegame.js
+++ src/dicegame.js
@@ -2,12 +2,14 @@
 function Dice() {}
 Dice.prototype.add = function(dice_calc) {
 	let checkArr = [1, 2, 3, 4, 5];
-	let checkArr2 = [2, 2, 3, 3];
 	// compare the array with the array [1, 2, 3, 4, 5]
 	if (JSON.stringify(dice_calc) === JSON.stringify(checkArr)) {
 		return 6;
 	}
-	else if (JSON.stringify(dice_calc) === JSON.stringify(checkArr2)) {
+
+	// compare the array with the array [2, 2, 3, 3]
+	let checkArr2 = [2, 2, 3, 3];
+	if (JSON.stringify(dice_calc) === JSON.stringify(checkArr2)) {
 		return 4;
 	}
 };
