d272a803601dfea9ab24ff831a35aca80c329600
nbrandtner-tgm <nbrandtner@gmx.at>
2024-05-20 20:31:07 +0200

    second test case added

--- spec/dicegame_spec.js
+++ spec/dicegame_spec.js
@@ -2,9 +2,15 @@ describe('Dice', function() {
 	beforeEach(function() {
 		dice = new Dice();
 	});
+
+	// Test Case 1:
 	let array1 = [1, 2, 3, 4, 5];
 	it('should return 6 for [ 1, 2, 3, 4, 5 ]', function() {
-		
 		expect(dice.add(array1)).toEqual(6);
+	});
+	// Test Case 2:
+	let array2 = [ 2, 2, 3, 3 ];
+	it('should return 4 for [ 2, 2, 3, 3 ]', function() {
+		expect(dice.add(array2)).toEqual(4);
 	});
 });
