3f26dd99fd992a3f7349f5640049339549bc8d30
ShadyMate <luisboehler@proton.me>
2024-05-20 20:44:36 +0200

    Prod code for second failing Test (Luis)

--- src/dicegame.js
+++ src/dicegame.js
@@ -2,8 +2,12 @@
 function Dice() {}
 Dice.prototype.add = function(dice_calc) {
 	let checkArr = [1, 2, 3, 4, 5];
+	let checkArr2 = [2, 2, 3, 3];
 	// compare the array with the array [1, 2, 3, 4, 5]
 	if (JSON.stringify(dice_calc) === JSON.stringify(checkArr)) {
 		return 6;
+	}
+	else if (JSON.stringify(dice_calc) === JSON.stringify(checkArr2)) {
+		return 4;
 	}
 };
